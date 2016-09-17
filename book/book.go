6a263038bc8e0a2a7948465bcca5851165f12276
mofon <yzkf000@foxmail.com>
2016-09-17 14:49:37 +0800

    fmt: book/book.go

--- book/book.go
+++ book/book.go
@@ -1,12 +1,12 @@
 package book
 
 type Book struct {
-	Author string `json:"author"`
-	Name string `json:"name"`
+	Author   string    `json:"author"`
+	Name     string    `json:"name"`
 	Chapters []Chapter `json:"chapters"`
 }
 
 type Chapter struct {
-	Title string `json:"title"`
+	Title   string `json:"title"`
 	Content string `json:"content"`
 }
