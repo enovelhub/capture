ddfd33b95dace26973ea84bc74a516c1ebd42253
mofon <yzkf000@foxmail.com>
2016-09-17 15:21:12 +0800

    add charset.ToUTF8(http.Responce) (io.Reader,error)

--- charset/charset.go
+++ charset/charset.go
@@ -1,10 +1,15 @@
 package charset
 
 import (
-	"io"
 	"golang.org/x/net/html/charset"
+	"io"
+	"net/http"
 )
 
 func NewReader(r io.Reader, contentType string) (io.Reader, error) {
-	return charset.NewReader(r,contentType)
+	return charset.NewReader(r, contentType)
+}
+
+func ToUTF8(resp *http.Response) (io.Reader, error) {
+	return NewReader(resp.Body, resp.Header.Get("Content-Type"))
 }
